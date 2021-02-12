69030240b698553284a53e0b3b0a40d95adc7eaf
Kohei Miyamoto <kouheimf@gmail.com>
2021-02-12 14:17:57 +0000

    comments added to app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-//----- fucntion using recursive -----
+//----- fibonacci fucntion using recursive -----
 // function fib(n){
 //     if(n === 0){
 //         return 0;
@@ -16,19 +16,32 @@
 // }
 
 
-//funtion with map
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 1);
+//funtion with Map object
+const memo = new Map(); //new map object
+memo.set(0, 0); //set 0
+memo.set(1, 1); //set 1
+
 function fib(n) {
   if (memo.has(n)) {
+    /* validation
+    if n = 0 or 1, return as they are
+    this is validation for fibonacci formula
+    */
     return memo.get(n);
   }
+
+  /* if n >= 2, store the value into map
+   */
   const value = fib(n - 1) + fib(n - 2);
-  memo.set(n, value);
+  memo.set(n, value); 
   return value;
 }
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
+
+/* the code with Map object can execute code almost 20 times faster
+than code with jsut recursive function.
+because it can store the calculated value inside the map*/
